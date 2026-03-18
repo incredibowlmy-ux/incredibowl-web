9da514f384641018eb839d1b9d980c27cf36aab5
Incredibowl.MY <incredibowl@gmail.com>
2026-03-19 06:37:00 +0800

    chore: exclude private routes from sitemap and robots.txt

--- next-sitemap.config.js
+++ next-sitemap.config.js
@@ -1,6 +1,16 @@
 /** @type {import('next-sitemap').IConfig} */
 module.exports = {
   siteUrl: 'https://www.incredibowl.my',
-  generateRobotsTxt: true, // (optional)
-  generateIndexSitemap: false, // 禁用索引，直接输出 URL
+  generateRobotsTxt: true,
+  generateIndexSitemap: false,
+  exclude: ['/admin*', '/checkout*', '/member*', '/account*', '/login*', '/icon.png'],
+  robotsTxtOptions: {
+    policies: [
+      {
+        userAgent: '*',
+        allow: '/',
+        disallow: ['/admin', '/checkout', '/member', '/account', '/login'],
+      },
+    ],
+  },
 }
